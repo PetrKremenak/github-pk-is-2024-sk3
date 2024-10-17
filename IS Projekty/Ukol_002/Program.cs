fd500ea2a3d26a0abd03dcef350dd64898f81bf0
PetrKremenak <vanishak00@gmail.com>
2024-10-17 11:44:30 +0200

    hodyhody

--- IS Projekty/Ukol_002/Program.cs
+++ IS Projekty/Ukol_002/Program.cs
@@ -1,10 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using
-namespace Soucet_cisel_zpusob1{
-class Program{
-    static void main{
-int a, b,;
+using System;
 
+namespace Scitani_metoda1 {
+    class Program {
+        static void Main()
+        {
+          Console.Write("Enter Username: ");
+          string Username = Console.ReadLine();
+          Console.WriteLine("Your username is " + Username);
+          Console.Write("Enter password: ");
+          string password = Console.ReadLine();
+          if (password == "Pica") {
+            Console.WriteLine("Úspěšně přihlášen");
+            }
+            else Console.WriteLine("Špatné heslo");
+          
+        }
     }
-}
 }
