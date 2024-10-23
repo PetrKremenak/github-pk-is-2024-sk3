246dd2cfa8025418471b058a78664f82f1bbcd88
PetrKremenak <vanishak00@gmail.com>
2024-10-23 20:07:47 +0200

    lol

--- IS Projekty/Úkol_003/Program.cs
+++ IS Projekty/Úkol_003/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+    class Program
+    {
+        static void Main ()
+        {//int x, y, i;
+    Console.WriteLine("Zadejte x");
+    int x = Convert.ToInt32(Console.ReadLine());
+    //Console.WriteLine("Zadejte y");
+    //int y = Convert.ToInt32(Console.ReadLine(14));
+    
+    for (int i = 0; i <= x; i++){
+
+        for (int j = 0; j < i; j++){
+            Console.Write("_ ");
+        }
+        Console.WriteLine();
+    }
+
+        } 
+        
+    }
+   
+
+
+
