3fad99d6fa5057e282fbd5894e69258e9754a3ef
PetrKremenak <vanishak00@gmail.com>
2024-10-23 16:55:06 +0200

    doit

--- IS Projekty/Ukol_002/obj/Debug/net8.0/github-pk-is-2024-sk3.AssemblyInfo.cs
+++ IS Projekty/Ukol_002/obj/Debug/net8.0/github-pk-is-2024-sk3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("github-pk-is-2024-sk3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6ec31e530dd403bbca4ed381590a8a82947a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd500ea2a3d26a0abd03dcef350dd64898f81bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("github-pk-is-2024-sk3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("github-pk-is-2024-sk3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
