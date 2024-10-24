7dec52f0e0ababab6e5325f48c0e672aac4a9139
PetrKremenak <vanishak00@gmail.com>
2024-10-24 16:26:54 +0200

    udelejto

--- IS Projekty/Úkol_003/Program.cs
+++ IS Projekty/Úkol_003/Program.cs
@@ -2,11 +2,11 @@
     class Program
     {
         static void Main ()
-        {//int x, y, i;
+        {
     Console.WriteLine("Zadejte x");
     int x = Convert.ToInt32(Console.ReadLine());
-    //Console.WriteLine("Zadejte y");
-    //int y = Convert.ToInt32(Console.ReadLine(14));
+    Console.WriteLine("Zadejte y");
+    int y = Convert.ToInt32(Console.ReadLine());
     
     for (int i = 0; i <= x; i++){
 
@@ -14,10 +14,18 @@
             Console.Write("_ ");
         }
         Console.WriteLine();
+
+    for (int z = 0; z <= x; y++){
+
+        for (int p = 0; p <= y; p++){
+            Console.Write("_ ");
+        }
+        Console.WriteLine();
     }
 
         } 
         
+    }
     }
    
 
